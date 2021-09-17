76caba3cecdcdcc0c0e6feb265996431d507af54
Faiiss <90894837+Faiiss@users.noreply.github.com>
2021-09-17 10:57:05 +0200

    Player stats

---
+++ PYTB1L2PlayerStatistics/Playerstatsfaiss.py
@@ -0,0 +1,10 @@
+Playername = "Player: Faissal"
+Shooting = "Shooting: 88"
+Pace = "Pace: 92"
+Passing = "Passing: 85"
+Fysiek = "Fysiek: 75"
+Dribbiling = "Dribbiling: 89"
+Defending = "Defending: 80" 
+
+
+print(Playername,Pace, Shooting, Passing, Dribbiling, Fysiek, Defending)
