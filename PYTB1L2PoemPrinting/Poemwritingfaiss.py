b09778bb35211edcb9a52a10ab8d1a4eaff71a5d
Faiiss <90894837+Faiiss@users.noreply.github.com>
2021-09-17 10:49:14 +0200

    Opdracht Poem writing

---
+++ PYTB1L2PoemPrinting/Poemwritingfaiss.py
@@ -0,0 +1,7 @@
+vers1 = "Ik ben Faissal"
+vers2 = "Op straat speel ik met een bal"
+vers3 = "Als ik de bal schiet hoor je knal"
+vers4 = "Maar we kunnen je prikken net als een kwal"
+# etc.....
+
+print(vers1,vers2, vers3, vers4)
