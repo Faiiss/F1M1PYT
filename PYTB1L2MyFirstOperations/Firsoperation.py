d3277efc93161a0ab15045c70269a647e4c5e6e0
Faiss <33278@ma-web.nl>
2021-09-22 12:56:52 +0200

    Operationopdracht

---
+++ PYTB1L2MyFirstOperations/Firsoperation.py
@@ -0,0 +1,8 @@
+a = 50
+b = 50
+print (a+b)
+print (b * 5) 
+print (b / a)
+print (a / 2 + b * 3)
+
+g
--- opdrachthellyou3.py
+++ opdrachthellyou3.py
@@ -2,3 +2,17 @@ print("Hell you! ik ben Faissal")
 
 x = input("Wie ben jij?")
 print ('Hello,' + x)
+
+import datetime
+import os
+x = datetime.datetime.now()
+
+print ("De datum van vandaag is:")
+print (x)
+
+while True:
+if (input("Want to repeat?") == "Y" ):
+    os.system('cls')
+    continue
+else:
+    break
