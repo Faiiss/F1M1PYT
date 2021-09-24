8df4e50927d41616d9b68654be64009c1dd5ab49
Faiss <33278@ma-web.nl>
2021-09-24 09:11:05 +0200

    Tell me more opdracht

---
+++ PYTB1L2InterestingTellMeMore/stelvragen.py
@@ -0,0 +1,14 @@
+score = 0 
+    
+
+print ("Hello you!")
+print ("Ik ben Faissal")
+
+x = input("Wie ben jij?")
+print ('Hello,' + x)
+
+b = input("Hoe oud ben je?")
+print ('Ah je bent,' + b)
+
+c = input("Waar woon je?")
+print ('Oh leuke plek ,' + c)
