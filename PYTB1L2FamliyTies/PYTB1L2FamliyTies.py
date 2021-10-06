097df30ef76d062be077c430db31e1edaa6dd33d
Faiss <33278@ma-web.nl>
2021-10-06 12:02:32 +0200

    Famliyties

---
+++ PYTB1L2FamliyTies/PYTB1L2FamliyTies.py
@@ -0,0 +1,7 @@
+name = ("Faissal")
+leeftijd = ("17")
+Afstand = ("4.5")
+
+print ("Mijn naam is" + (name) + " ik ben" + (leeftijd) + "jaar oud en woon in Amsterdam") 
+print ("ik heb een eigen bedrijf, ik doe textiel en andere producten bedrukken voor klanten")
+print( "Ik woon kilometer" + (Afstand) + "van het Mediacollege")
