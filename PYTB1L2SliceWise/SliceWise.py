c84d2f35f45523dae91c2f77481b8c3131e285c9
Faiss <33278@ma-web.nl>
2021-10-06 12:30:45 +0200

    SliceWise

---
+++ PYTB1L2SliceWise/SliceWise.py
@@ -0,0 +1,25 @@
+naam = "Faiss is the man"
+voornaam = naam[0:5]
+print(voornaam)
+
+# Opdracht 1
+
+tekst = 'De Python lessen worden gegeven voor Erik, Erwin en ook door Hidde'
+namen = tekst[36:41] 
+namen1 = tekst[41:49]
+namen2 = tekst[60:66]
+print(namen + namen1 + namen2)
+
+#Opdracht 2
+
+tekst2 = 'SD vakken zijn bijvoorbeeld Python, UXD, Frontend development en Backend development. Wat leuk!'
+
+vak1 = tekst2[27:61]
+vak2 = tekst2[64:84]
+print(vak1+vak2)
+
+#Opdracht 3
+
+tekst3 = 'Wat is hier het laatste woord?'
+zas=tekst3[20:27]
+print(zas)
