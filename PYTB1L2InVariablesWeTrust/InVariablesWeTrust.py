9108cb436bbea890544ff8b6b4f052cef83a18af
Faiss <33278@ma-web.nl>
2021-10-06 13:08:04 +0200

    InVariablesWeTrust

---
+++ PYTB1L2InVariablesWeTrust/InVariablesWeTrust.py
@@ -0,0 +1,23 @@
+Character = "#"
+
+Float = 2
+
+Boolean1 = False
+Boolean2 = True
+
+Interger1 = 69
+Interger2 = 21
+Interger3 = Interger1 - Interger2
+
+String1 = "Hallo" 
+String2 = "Jatoch"
+String3 = String1 + String2
+
+print(Character)
+print(Float)
+print(Boolean2)
+print(Interger3)
+print(String3)
+
+
+
