6375d96517fe59c63b83b57c024e555f7f524da9
Faiss <33278@ma-web.nl>
2021-10-27 11:45:33 +0200

    HandleTheList

---
+++ PYTB1L6CanYouHandleTheList/CanYouHandleTheList.py
@@ -0,0 +1,21 @@
+lijst = ["MA", False, 2004, "coderen duurt te lang"]
+
+print(lijst)
+
+lijst.append("Ewa")
+
+print(lijst)
+
+lijst.remove(False)
+lijst.remove(2004)
+lijst.remove("coderen duurt te lang")
+
+print(lijst)
+
+lijst.append("Random")
+
+lijst.sort()
+
+lijst.append("Geen inspiratie")
+
+print(lijst)
